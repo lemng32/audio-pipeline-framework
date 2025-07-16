315bf3003054356167294c994e0b5aba7d351bad
Lam Nguyen <nguyenlm@miamioh.edu>
2025-07-16 11:28:26 +0700

    Implemented function to segment diarize results. Needs further testing

--- vivoice_preprocess/preprocessor.py
+++ vivoice_preprocess/preprocessor.py
@@ -38,7 +38,7 @@ class PreprocessorPipeline:
     Run the audio preprocessing pipeline.
 
     Args:
-      audio (Union[np.ndarray, Path]): Audio waveform or path to audio file.
+      audio (Union[np.ndarray, Path]): Audio waveform, path to audio file or pydub's AudioSegment.
       sample_rate (int): Audio sample rate.
 
     Raises:
@@ -59,26 +59,29 @@ class PreprocessorPipeline:
         raise FileNotFoundError(f"Audio file does not exist: {audio}")
     elif isinstance(audio, np.ndarray):
       if audio.ndim > 2:
-        raise ValueError(f"Expected mono or stereo audio array, got shape: {audio.shape}")
-    
+        raise ValueError(
+          f"Expected mono or stereo audio array, got shape: {audio.shape}"
+        )
+
     if not sample_rate:
-      raise ValueError("Sample rate of audio not provided")
-    
+      raise ValueError("Sample rate of audio not provided.")
+
     # Step 1: Standardization
     audio = self.standardization(audio=audio, sample_rate=sample_rate)
 
     # Step 2: Diarization
     diarize_df = self.diarization(audio)
+    segment_list = self.segment_by_speaker(diarize_df)
 
     # Step 3: ASR
-    segments, info = self.whisper_asr(audio)
+    asr_res = self.whisper_asr(segment_list, audio)
 
     # TO-DO: Modify return type for general use. Currently return everything as a dict.
     return {
       "audio": audio,
       "diarize_df": diarize_df,
-      "segments": segments,
-      "info": info,
+      "segment_list": segment_list,
+      "asr_res": asr_res,
     }
 
   # Step 1: Standardization
@@ -89,7 +92,7 @@ class PreprocessorPipeline:
     Standardize audio by resampling, normalizing volume, converting to mono, and extracting waveform.
 
     Args:
-      audio (Union[np.ndarray, Path]): Input audio as a NumPy array or file path.
+      audio (Union[np.ndarray, Path]): Audio waveform, path to audio file or pydub's AudioSegment.
       sample_rate (int): Desired sample rate.
 
     Returns:
@@ -124,7 +127,6 @@ class PreprocessorPipeline:
       gain = target_dBFS - audio.dBFS
 
       normalized_audio = audio.apply_gain(min(max(gain, -3), 3))
-
       waveform = np.array(normalized_audio.get_array_of_samples(), dtype=np.float32)
 
     max_amplitude = np.max(np.abs(waveform))
@@ -158,10 +160,9 @@ class PreprocessorPipeline:
         "channel": 0,
       }
     )
-    annotation_iter = annotation.itertracks(yield_label=True)
 
     diarize_df = pd.DataFrame(
-      data=annotation_iter,
+      data=annotation.itertracks(yield_label=True),
       columns=["segment", "track", "speaker"],
     )
     diarize_df["start"] = diarize_df["segment"].apply(lambda x: x.start)
@@ -169,8 +170,51 @@ class PreprocessorPipeline:
 
     return diarize_df
 
+  def segment_by_speaker(self, diarize_df: pd.DataFrame):
+    MERGE_GAP = 2  # merge gap in seconds, if smaller than this, merge
+    MIN_SEGMENT_LENGTH = 5  # min segment length in seconds
+    MAX_SEGMENT_LENGTH = 30  # max segment length in seconds
+
+    updated_list = []
+
+    for _, row in diarize_df.iterrows():
+      segment = {
+        "start": row["start"],
+        "end": row["end"],
+        "speaker": row["speaker"]
+      }
+
+      last_start_time = updated_list[-1]["start"] if updated_list else None
+      last_end_time = updated_list[-1]["end"] if updated_list else None
+      last_speaker = updated_list[-1]["speaker"] if updated_list else None
+
+      if segment["end"] - segment["start"] >= MAX_SEGMENT_LENGTH:
+        print("Cac")
+
+      if (
+        last_speaker is None
+        or last_speaker != segment["speaker"]
+        or segment["end"] - segment["start"] >= MIN_SEGMENT_LENGTH
+      ):
+        updated_list.append(segment)
+        continue
+
+      if (
+        segment["start"] - last_end_time >= MERGE_GAP
+        or segment["end"] - last_start_time >= MAX_SEGMENT_LENGTH
+      ):
+        updated_list.append(segment)
+      else:
+        updated_list[-1]["end"] = segment["end"]
+
+    filtered_list = [
+      seg for seg in updated_list if seg["end"] - seg["start"] >= MIN_SEGMENT_LENGTH
+    ]
+
+    return filtered_list
+
   # Step 3: Whipser ASR
-  def whisper_asr(self, audio: dict, options: Optional[dict] = None) -> tuple:
+  def whisper_asr(self, segment_list, audio: dict, options: Optional[dict] = None) -> tuple:
     """
     Run Whisper ASR model on the given audio.
 
@@ -193,6 +237,26 @@ class PreprocessorPipeline:
     if options:
       transcription_options.update(options)
 
-    audio_waveform = librosa.resample(audio["waveform"], orig_sr=audio["sample_rate"], target_sr=16000)
+    res = []
+    sample_rate = audio["sample_rate"]
+    audio_waveform = librosa.resample(
+      audio["waveform"], orig_sr=sample_rate, target_sr=16000
+    )
 
-    return self.model.transcribe_audio(audio=audio_waveform, **transcription_options)
+    for seg in segment_list:
+      start_frame = int(seg["start"] * sample_rate)
+      end_frame = int(seg["end"] * sample_rate)
+
+      tmp_audio = audio_waveform[start_frame:end_frame]
+
+      segments, info = self.model.transcribe_audio(tmp_audio, **transcription_options)
+
+      res.append({
+        "start": seg["start"],
+        "end": seg["end"],
+        "speaker": seg["speaker"],
+        "segments": segments,
+        "info": info,
+      })
+
+    return res
